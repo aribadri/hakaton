f25945eaebff88a6894687f79347e6e723910a6e
Arian <arian@MB-Pro-M2---Arian.local>
2025-09-29 19:32:05 +0300

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,28 @@ import { defineConfig } from "vite";
 import fs from "fs";
 import path from "path";
 
-export default defineConfig({
-  root: "src/",
-  publicDir: "../static/",
-  base: "./",
-  server: {
-    host: true,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "certs/localhost-key.pem")),
-      cert: fs.readFileSync(path.resolve(__dirname, "certs/localhost.pem")),
+export default defineConfig(({ command }) => {
+  const isDev = command === "serve";
+
+  return {
+    root: "src/",
+    publicDir: "../static/",
+    base: "./",
+    server: {
+      host: true,
+      ...(isDev && {
+        https: {
+          key: fs.readFileSync(
+            path.resolve(__dirname, "certs/localhost-key.pem")
+          ),
+          cert: fs.readFileSync(path.resolve(__dirname, "certs/localhost.pem")),
+        },
+      }),
     },
-  },
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-    sourcemap: true,
-  },
+    build: {
+      outDir: "../dist",
+      emptyOutDir: true,
+      sourcemap: true,
+    },
+  };
 });
