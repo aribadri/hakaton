54186d19d881eb48c3bdb3aca2c0bae6ca889e5d
GeorgeStudio96 <geo.erh96@gmail.com>
2025-10-02 10:50:25 +0300

    cta has been added

--- src/components/screenshot.js
+++ src/components/screenshot.js
@@ -41,6 +41,16 @@ const getScreenshot = {
     const resultImg = document.createElement("img");
     resultImg.id = "resultImg";
 
+    // CTA плашка
+    const ctaBanner = document.createElement("div");
+    ctaBanner.className = "cta-banner";
+    ctaBanner.innerHTML = `
+      <svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M9 5H7C5.89543 5 5 5.89543 5 7V19C5 20.1046 5.89543 21 7 21H17C18.1046 21 19 20.1046 19 19V7C19 5.89543 18.1046 5 17 5H15M9 5C9 6.10457 9.89543 7 11 7H13C14.1046 7 15 6.10457 15 5M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5M12 12H15M12 16H15M9 12H9.01M9 16H9.01" stroke="white" stroke-width="2" stroke-linecap="round"/>
+      </svg>
+      <span>Предъяви фото на кассе<br>и получи скидку 10%</span>
+    `;
+
     const backBtn = document.createElement("button");
     backBtn.className = "back-btn hidden";
     backBtn.innerHTML = `
@@ -50,6 +60,7 @@ const getScreenshot = {
     `;
 
     resultImgWrapper.appendChild(resultImg);
+    resultImgWrapper.appendChild(ctaBanner);
     resultImgWrapper.appendChild(backBtn);
     document.body.appendChild(resultImgWrapper);
 
